4a410c7c8d73d09eb8ae75ff95d32de14b6221bf
allankeivnscain <meuemail44allan@gmail.com>
2025-05-14 11:40:58 -0300

    feat: criação do modelo de area e migration

--- vagas_e_candidatos/models.py
+++ vagas_e_candidatos/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+AREA_CHOICES = [
+    ('adm', 'Administração'),
+    ('eng', 'Engenharia'),
+    ('tec', 'Tecnologia da Informação'),
+    ('edu', 'Educação'),
+    ('jur', 'Direito'),
+    ('med', 'Medicina'),
+    ('enf', 'Enfermagem'),
+    ('fin', 'Finanças'),
+    ('mkt', 'Marketing'),
+    ('com', 'Comércio'),
+    ('arq', 'Arquitetura'),
+    ('des', 'Design'),
+    ('coms', 'Comunicação Social'),
+    ('log', 'Logística'),
+    ('rec', 'Recursos Humanos'),
+    ('tur', 'Turismo'),
+    ('agr', 'Agronomia'),
+    ('vet', 'Medicina Veterinária'),
+    ('art', 'Artes'),
+    ('out', 'Outros'),
+]
+
+TIPO_CONTRATO_CHOICES = [
+    ('clt', 'CLT (Efetivo)'),
+    ('pj', 'Pessoa Jurídica (PJ)'),
+    ('est', 'Estágio'),
+    ('tra', 'Temporário'),
+    ('fre', 'Freelancer'),
+    ('apr', 'Aprendiz'),
+    ('aut', 'Autônomo'),
+    ('vol', 'Voluntário'),
+    ('int', 'Intermitente'),
+    ('out', 'Outros'),
+]
+
+class Vaga(models.Model):
+    titulo = models.CharField(max_length=200)
+    area = models.CharField(max_length=50, choices=AREA_CHOICES)
+    tipo_contrato = models.CharField(max_length=50, choices=TIPO_CONTRATO_CHOICES)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.titulo
